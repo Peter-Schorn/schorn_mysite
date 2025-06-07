9b4f633f825e6459f8b9ea15181fbdfe1a0fbfa7
Peter Schorn <petervschorn@gmail.com>
2025-06-07 16:23:10 -0500

    First commit.

---
+++ testbench/first/script.js
@@ -0,0 +1,39 @@
+// {
+//     insideFn();
+//     function outsideFn() {
+//         function insideFn() {
+//             // commands;
+//         }
+//         // commands;
+//     }
+// }
+
+// {
+//     function outsideFn() {
+//         // commands
+// insideFn function () {
+//             // commands;
+//         }
+//         // // commands;
+//     }
+// }
+
+// {
+//     outsideFn()function {
+//         // commands;
+//         function insideFn() {
+//             // commands;
+//         }
+//         // commands;
+//     }
+// }
+
+{
+    function outsideFn() {
+        // commands;
+        function insideFn() {
+            // commands;
+        }
+        // commands;
+    }
+}
